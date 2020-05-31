e0781c487d5b53a5dc5594f3732712dc2fd5dcd1
XenixIT <66084029+XenixIT@users.noreply.github.com>
2020-05-31 15:32:59 +0600

    Create BlackJack.py

---
+++ BlackJack.py
@@ -0,0 +1,27 @@
+import random
+koloda = [6,7,8,9,10,1,2,3,4,11] * 4
+random.shuffle(koloda)
+
+print('Поиграем в BlackJack?')
+count = 0
+
+while True:
+	choice = input('Будете брать карту? y/n\n') 
+	choice == 'y'
+	if choice == 'y':
+		current = koloda.pop()
+		print('                             Вам попала карта достоинством %d' %current)
+		count += current
+		if count > 21:
+			print('                             Извините но вы проиграли!')
+			break
+		elif count == 21:
+			print('                         Поздравляю вы набрали 21!')
+			break
+		else:
+			print('                             У вас %d очков.' %count)
+	elif choice == 'n': 
+		print('                                 У вас %d очков и вы закончили игру.' %count)
+		break
+
+print('                                До новых встреч')
